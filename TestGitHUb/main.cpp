2228f931cf426204059d2cf7559cfc5991a1fff1
Alessandro Avellone <alessandro.avellone@unimib.it>
2025-01-15 10:33:37 +0100

    Initial Commit

---
+++ TestGitHUb/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TestGitHUb
+//
+//  Created by Alessandro Avellone on 15/01/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
